22cf7c7c4bd813fa0a79759696752a7e03ff8339
yanglei <yuryqwer@outlook.com>
2023-09-19 11:37:44 +0800

    Chapter14.1 Unit testing and sub-tests

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/yuryqwer/snippetbox/internal/assert"
+)
+
+func TestHumanDate(t *testing.T) {
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2023, 9, 19, 9, 42, 0, 0, time.UTC),
+			want: "19 Sep 2023 at 09:42",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2022 at 09:15",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+
+			assert.Equal(t, hd, tt.want)
+		})
+	}
+}
