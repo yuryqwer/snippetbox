b46defb471dac85ce2c97f21af26cf18cc004cc9
yanglei <yuryqwer@outlook.com>
2023-09-20 14:04:03 +0800

    Chapter17.4 Add an 'Account' page to the application

--- internal/models/mocks/user.go
+++ internal/models/mocks/user.go
@@ -1,6 +1,10 @@
 package mocks
 
-import "github.com/yuryqwer/snippetbox/internal/models"
+import (
+	"time"
+
+	"github.com/yuryqwer/snippetbox/internal/models"
+)
 
 type UserModel struct{}
 
@@ -28,4 +32,19 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	default:
 		return false, nil
 	}
+}
+
+func (m *UserModel) Get(id int) (*models.User, error) {
+	if id == 1 {
+		u := &models.User{
+			ID:      1,
+			Name:    "Alice",
+			Email:   "alice@example.com",
+			Created: time.Now(),
+		}
+
+		return u, nil
+	}
+
+	return nil, models.ErrNoRecord
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -15,6 +15,7 @@ type UserModelInterface interface {
 	Insert(name, email, password string) error
 	Authenticate(email, password string) (int, error)
 	Exists(id int) (bool, error)
+	Get(id int) (*User, error)
 }
 
 type User struct {
@@ -87,4 +88,21 @@ func (m *UserModel) Exists(id int) (bool, error) {
 
 	err := m.DB.QueryRow(stmt, id).Scan(&exists)
 	return exists, err
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	var user User
+
+	stmt := `SELECT id, name, email, created FROM users WHERE id = ?`
+
+	err := m.DB.QueryRow(stmt, id).Scan(&user.ID, &user.Name, &user.Email, &user.Created)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
